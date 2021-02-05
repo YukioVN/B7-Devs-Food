0d341ade817a3f775fdc235cd121cb0400f42ec3
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-05 17:49:21 -0300

    Imported Header component in Home Page

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
-import { Container, Titulo } from './styled';
+import { Container } from './styled';
+
+import Header from '../../components/Header';
 
 export default () => {
-    const history = useHistory();
+  const history = useHistory();
+  const [headerSearch, setHeaderSearch] = useState('');
 
-    const handleButtonClick = () => {
-        history.push('/tela2/testador');
-    }
-
-    return (
-        <Container>
-            <Titulo>Homepage</Titulo>
-            <button onClick={handleButtonClick}>Ir para Tela 2</button>
-        </Container>
-    );
+  return (
+    <Container>
+      <Header search={headerSearch} onSearch={setHeaderSearch} />
+    </Container>
+  );
 }

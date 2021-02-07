46d2064ff5673117ca0834d5f6bcaaf7ecd4d3af
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-07 17:07:43 -0300

    Added subtitles with react-tooltip in menu components

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { LinkArea, LinkIcon } from './styled';
 
-export default ({icon, link}) => {
+export default ({title, icon, link}) => {
   const history = useHistory();
   const location = useLocation();
 
@@ -14,7 +14,13 @@ export default ({icon, link}) => {
   }
 
   return (
-    <LinkArea active={isActive} href={link} onClick={handleLinkClick}>
+    <LinkArea 
+      data-tip={title}
+      data-for='tip-right'
+      active={isActive} 
+      href={link} 
+      onClick={handleLinkClick}
+    >
       <LinkIcon  src={icon} />
     </LinkArea>
   );

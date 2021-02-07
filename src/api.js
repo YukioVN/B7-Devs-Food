c6f17afb9b3021c6fc9b9d1500a6c077a356bf5f
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-07 19:07:49 -0300

    Added Products requisition in api

--- src/api.js
+++ src/api.js
@@ -6,5 +6,26 @@ export default {
     const res = await fetch(BASE+'/categories');
     const json = await res.json();
     return json;
+  },
+  getProducts: async (category, page, search) => {
+    //GET / products([search, page, category])
+    let fields = {};
+    if(category!== 0) {
+      fields.category = category;
+    }
+
+    if(page > 0) {
+      fields.page = page;
+    }
+
+    if(search != '') {
+      fields.search = search;
+    }
+    
+    let queryString = new URLSearchParams(fields).toString();
+
+    const res = await fetch(BASE+'/products?'+queryString);
+    const json = await res.json();
+    return json;
   }
 };

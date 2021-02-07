bc170b04ebf7ca53830eeb9401ee30f5af5dc3a2
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-07 17:12:18 -0300

    Added Api file

---
+++ src/api.js
@@ -0,0 +1,10 @@
+let BASE = 'https://api.b7web.com.br/devsfood/api';
+
+export default {
+  getCategories: async () => {
+    //GET /categories
+    const res = await fetch(BASE+'/categories');
+    const json = await res.json();
+    return json;
+  }
+};

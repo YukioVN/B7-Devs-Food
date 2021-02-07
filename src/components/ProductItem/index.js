611b710dc976c31c88934087c8836a5ed3149afa
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-07 19:05:02 -0300

    Created and styled Products Component

---
+++ src/components/ProductItem/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { 
+  Container, 
+  ProductPhotoArea, 
+  ProductInfoArea, 
+  ProductButtonArea,
+  ProductPhoto,
+  ProductName,
+  ProductPrice,
+  ProductIngredients,
+  ProductButton
+} from './styled';
+
+export default ({data}) => {
+  return (
+    <Container>
+      <ProductPhotoArea>
+        <ProductPhoto src={data.image} />
+      </ProductPhotoArea>
+      <ProductInfoArea>
+        <ProductName>{data.name}</ProductName>
+        <ProductPrice>R$ {data.price}</ProductPrice>
+        <ProductIngredients>{data.ingredients}</ProductIngredients>
+      </ProductInfoArea>
+      <ProductButtonArea>
+        <ProductButton src='/assets/Devsfood_img/next.png' />
+      </ProductButtonArea>
+    </Container>
+  );
+}
---
+++ src/components/ProductItem/styled.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #FFF;
+  border-radius: 5px;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  color: #136713;
+  cursor: pointer;
+`;
+
+export const ProductPhotoArea = styled.div`
+  width: 100px;
+`; 
+
+export const ProductInfoArea = styled.div`
+  flex: 1px;
+  margin-left: 10px;
+  margin-right: 10px;
+`; 
+
+export const ProductButtonArea = styled.div`
+
+`;
+
+export const ProductPhoto = styled.img`
+    width: 100%;
+
+`;
+
+export const ProductName = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const ProductPrice = styled.div`
+  font-size: 14px;
+`;
+
+export const ProductIngredients = styled.div`
+  font-size: 11px;
+`;
+  
+export const ProductButton = styled.img`
+  width: 15px;
+`;

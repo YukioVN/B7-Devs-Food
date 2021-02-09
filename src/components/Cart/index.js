940856fae2016541f14a2c525a5b5b9d24af855c
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-09 17:10:41 -0300

    Updated Cart component with adding and removing functions, and also styled the component

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,15 +1,74 @@
-import React from 'react';
-import { CartArea, CartHeader, CartIcon, CartBody, CartText } from './styled';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { 
+  CartArea, 
+  CartHeader, 
+  CartIcon, 
+  CartBody, 
+  CartText,
+  ProductsArea,
+  ProductItem,
+  ProductPhoto,
+  ProductInfoArea, 
+  ProductName,
+  ProductPrice, 
+  ProductQuantityArea,
+  ProductQtIcon,
+  ProductQtText
+} from './styled';
 
 export default () => {
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.cart.products);
+
+  const [show, setShow] = useState(true);
+  
+  const handleCartClick = () => {
+    setShow(!show);
+  }
+
+  const handleProductChange = (key, type) => {
+    dispatch({
+      type: 'CHANGE_PRODUCT',
+      payload:{key, type}
+    });
+  }
+
   return(
     <CartArea>
-      <CartHeader>
+      <CartHeader onClick={handleCartClick}>
         <CartIcon src='/assets/Devsfood_img/cart.png' />
-        <CartText>Menu Carrinho (X)</CartText>
+        <CartText>Menu Carrinho ({products.length})</CartText>
+        {show &&
+          <CartIcon src='/assets/Devsfood_img/down.png' />
+        }
       </CartHeader>
-      <CartBody>
+      <CartBody show={show}>
+        <ProductsArea>
+          {products.map((item, index)=>(
+            <ProductItem key={index}>
+              <ProductPhoto src={item.image} />
+              <ProductInfoArea>
+                <ProductName>{item.name}</ProductName>
+                <ProductPrice>R$ {item.price.toFixed(2)}</ProductPrice>
+              </ProductInfoArea>
+              <ProductQuantityArea>
+                <ProductQtIcon
+                  src='/assets/Devsfood_img/minus.png' 
+                  onClick={()=>handleProductChange(index, '-')}
+                />
+                <ProductQtText>{item.qt}</ProductQtText>
+                <ProductQtIcon 
+                  src='/assets/Devsfood_img/plus.png' 
+                  onClick={()=>handleProductChange(index, '+')} 
+                />
+
+              </ProductQuantityArea>
+            </ProductItem>
+          ))}
           
+        </ProductsArea>
       </CartBody>
     </CartArea>
   )
--- src/components/Cart/styled.js
+++ src/components/Cart/styled.js
@@ -25,10 +25,59 @@ export const CartIcon = styled.img`
 `;
 
 export const CartText = styled.div`
+  flex: 1;
   color: #FFF;
   font-size: 17px;
 `;
 
 export const CartBody = styled.div`
+  display: ${props=>props.show ? 'block' : 'none'};
+  color: #FFF;
+`;
 
+export const ProductsArea = styled.div`
+
+`;
+
+export const ProductItem = styled.div`
+  display: flex;
+  margin: 10px;
+`;
+
+export const ProductPhoto = styled.img`
+  width: 64px;
+  height: auto;
+  border-radius: 10px;
+  
+`;
+
+export const ProductInfoArea = styled.div`
+  flex: 1;
+  margin-left: 10px;
+`;
+
+export const ProductName = styled.div`
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const ProductPrice = styled.div`
+  font-size: 13px;
+`;
+
+export const ProductQuantityArea = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ProductQtIcon = styled.img`
+  width: 13px;
+  height: auto;
+  cursor: pointer;
+`;
+
+export const ProductQtText = styled.div`
+  font-size: 13px;
+  font-weight: bold;
+  margin: 0 5px;
 `;

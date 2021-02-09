e705d642d222075d991ac184151ee97d39b17c34
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-08 21:50:17 -0300

    Created Cart Reducer

---
+++ src/reducers/CartReducer.js
@@ -0,0 +1,38 @@
+const initialState = {
+  products:[],
+  address:[],
+  discount:0,
+  delivery:0
+};
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case 'ADD_PRODUCT':
+      let products = [...state.products];
+      let id = action.payload.data.id;
+
+      let index = products.findIndex(item => item.id === id);
+      if(index > -1) {
+        products[index].qt += action.payload.qt;
+      } else {
+        products.push({
+          ...action.payload.data,
+          qt: action.payload.qt
+        })
+      }
+
+      console.log(products);
+
+      return {...state, products}
+    break;
+      /*case 'SET_TOKEN':
+          return {...state, token: action.payload.token}
+      break;
+      
+      case 'SET_NAME':
+          return {...state, name: action.payload.name};
+      break;*/
+  }
+
+  return state;
+}

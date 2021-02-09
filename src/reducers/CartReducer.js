a641c1759e011a53d1fe349c963162307f26e4a2
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-09 17:11:52 -0300

    Created adding and removing functions in reducers

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -6,9 +6,10 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+  let products = [...state.products];
+
   switch(action.type) {
     case 'ADD_PRODUCT':
-      let products = [...state.products];
       let id = action.payload.data.id;
 
       let index = products.findIndex(item => item.id === id);
@@ -20,9 +21,26 @@ export default (state = initialState, action) => {
           qt: action.payload.qt
         })
       }
+      return {...state, products}
+    break;
 
-      console.log(products);
+    case 'CHANGE_PRODUCT':
+      if(products[action.payload.key]) {
+        switch(action.payload.type) {
+          case '-':
+            products[action.payload.key].qt--;
 
+            if(products[action.payload.key].qt <= 0) {
+              products = products.filter((item, index)=> index != action.payload.key)
+            }
+
+          break;
+  
+          case '+':
+              products[action.payload.key].qt++;
+          break;
+        }
+      }
       return {...state, products}
     break;
       /*case 'SET_TOKEN':

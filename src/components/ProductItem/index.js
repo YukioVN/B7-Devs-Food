66cefc61818d921f31281974ae59fe6560454174
YukioVN <noma@alunos.utfpr.edu.br>
2021-02-08 21:48:03 -0300

    Added onClick function to show modal

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -11,9 +11,13 @@ import {
   ProductButton
 } from './styled';
 
-export default ({data}) => {
+export default ({data, onClick}) => {
+  const handleClick = () => {
+    onClick(data);
+  }
+
   return (
-    <Container>
+    <Container onClick={handleClick}>
       <ProductPhotoArea>
         <ProductPhoto src={data.image} />
       </ProductPhotoArea>
